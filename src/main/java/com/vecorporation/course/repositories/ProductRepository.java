fd22a00afd48080f557a76163191782d671c38fb
Vinicius Elias <vini-mq@hotmail.com>
2023-08-30 18:15:51 -0300

    Product

---
+++ src/main/java/com/vecorporation/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.vecorporation.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.vecorporation.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+	
+}
